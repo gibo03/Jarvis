2e18f338b00ec53e42397315d66259692252c6d5
gibo03 <77393170+gibo03@users.noreply.github.com>
2021-06-21 17:20:56 +0200

    Add files via upload
    dodat ispis

--- Main.java
+++ Main.java
@@ -3,10 +3,32 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static Scanner ul = new Scanner(System.in);
+    static class Point {
+        int x;
+        int y;
+    }
+
+    public static class Jarvis {
+
+        public void ispisi(Point[] points, int[] next) {
+            System.out.println("\ntacke : ");
+
+            for (int i = 0; i < next.length; i++)
+                if (next[i] != -1) {
+                    System.out.println("( " + points[i].x + ", " + points[i].y + " )");
+                }
+        }
+
+       
+
+
+
+
+    }
 
 
     public static void main(String[] args) {
-        
 
     }
 }
