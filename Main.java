7dcbd0bece22127681a18f267a0607287ff0ffe9
gibo03 <77393170+gibo03@users.noreply.github.com>
2021-06-21 17:23:01 +0200

    Add files via upload
    dodat jarvis algoritam

--- Main.java
+++ Main.java
@@ -25,7 +25,27 @@ public class Main {
             return !(val >= 0);
         }
 
-        
+        public void Jarvis(Point[] points) {
+            if (points.length <= 2)
+                return;
+            int[] next = new int[points.length];
+            Arrays.fill(next, -1);
+
+            int lMax = 0;
+            for (int i = 1; i < points.length; i++)
+                if (points[i].x < points[lMax].x)
+                    lMax = i;
+            int m = lMax, n;
+            do {
+                n = (m + 1) % points.length;
+                for (int i = 0; i < points.length; i++)
+                    if (orijentacija(points[m], points[i], points[n]))
+                        n = i;
+                next[m] = n;
+                m = n;
+            } while (m != lMax);
+            ispisi(points, next);
+        }
 
 
     }
